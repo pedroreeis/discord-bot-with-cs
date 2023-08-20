f239f411914b0dfca70fc8c01067836566a8f396
Pedro <Pedro@DESKTOP-CEVPH14>
2023-08-20 01:22:39 -0300

    Adicionar arquivos de projeto.

---
+++ ClientConstructor.cs
@@ -0,0 +1,63 @@
+﻿using DiscordBot.Commands;
+using DiscordBot.Models;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace DiscordBot
+{
+    public class ClientConstructor
+    {
+
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+
+        public async Task Run()
+        {
+
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("settings.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+            json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigModel>(json);
+
+                var config = new DiscordConfiguration
+                {
+                    Token = configJson.Token,
+                    TokenType = TokenType.Bot,
+                    AutoReconnect = true,
+                    MinimumLogLevel = LogLevel.Debug
+                };
+
+            Client = new DiscordClient(config);
+            Client.Ready += onClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] {configJson.Prefix}, 
+                EnableMentionPrefix = true,
+                EnableDms = false,
+                DmHelp = true
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<FunCommands>();
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task onClientReady(DiscordClient client, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Commands/FunCommands.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Retorna a palavra Pong.")]
+        public async Task Ping(CommandContext ctx)
+        {
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Pong!",
+                Color = DiscordColor.Red,
+                Thumbnail =
+                {
+                    Url = ctx.Client.CurrentUser.AvatarUrl
+                },
+                Description = "É só isso kkkkkkkk",
+                Timestamp = DateTime.Now,
+                Footer =
+                {
+                    Text = "Isso é um comando para diversão, apenas."
+                }
+            };
+            await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);  
+        }
+    }
+}
